28ef98920fa013c988f20811607d336433714164
Sudharshan V <sudarshan61kv@gmail.com>
2022-03-26 10:47:29 +0530

    changes

--- iplBid/settings.py
+++ iplBid/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3i((eps*(0y%&df@8jiy&tu4wxt)2ny33$0_lqgcb_kf#7-elx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
