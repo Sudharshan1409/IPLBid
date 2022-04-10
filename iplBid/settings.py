d362441956d8971f3c4fd8d5e2e6fb15c5d91fdf
Sudharshan V <sudarshan61kv@gmail.com>
2022-04-10 16:44:13 +0530

    changed creds.json path

--- iplBid/settings.py
+++ iplBid/settings.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import json
-creds_file = open('creds.json', 'r')
-creds = json.load(creds_file)
-creds_file.close()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+creds_file = open(os.path.join(BASE_DIR, 'creds.json'), 'r')
+creds = json.load(creds_file)
+creds_file.close()
 
 
 # Quick-start development settings - unsuitable for production
